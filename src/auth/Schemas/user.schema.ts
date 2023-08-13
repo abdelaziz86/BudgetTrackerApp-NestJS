336a6ac61f8c807419334d05a1bcb76b90b6f04c
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-13 20:23:43 +0200

    auth 1

---
+++ src/auth/Schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+@Schema({
+    timestamps : true
+}) 
+
+export class User extends Document {
+    @Prop()
+    name: string;
+    
+    @Prop()
+    surname : string;
+
+    @Prop({ unique : [true , 'Email already exists']})
+    email : string;
+
+    @Prop()
+    password : string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
